8adb3feea91e61d1969f76d7225411707e175273
Koske Kimutai <koskedk@live.com>
2019-09-10 14:26:20 +0300

    fixed secrets folder

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     key: fs.readFileSync('./secrets/demo-key.pem'),
     cert: fs.readFileSync('./secrets/demo-cert.pem'),
   };
+
   const app = await NestFactory.create(AppModule, { httpsOptions });
   const config = app.get(ConfigService);
   const microservice = app.connectMicroservice(config.QueueConfig);
