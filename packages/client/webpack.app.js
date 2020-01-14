3cd189526c7fd8ef205cf1c30ea384da3f87ef79
Koske Kimutai <koskedk@live.com>
2020-01-14 17:48:56 +0300

    startup fix

--- packages/client/webpack.app.js
+++ packages/client/webpack.app.js
@@ -6,21 +6,20 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    entry: {
-        "main": path.join(APP_PATH, "index.tsx")
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(APP_PATH, "index.html"),
-            favicon: path.join(APP_PATH, "favicon.ico")
-        }),
-        new webpack.DefinePlugin({
-            'SERVICE_HOST': JSON.stringify('localhost')
-        })
-    ]
+  entry: {
+    main: path.join(APP_PATH, "index.tsx")
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(APP_PATH, "index.html"),
+      favicon: path.join(APP_PATH, "favicon.ico")
+    }),
+    new webpack.DefinePlugin({
+      SERVICE_HOST: JSON.stringify("localhost")
+    })
+  ]
 });
-
