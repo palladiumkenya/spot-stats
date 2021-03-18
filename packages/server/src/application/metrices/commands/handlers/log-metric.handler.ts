e17913e6b483852faa27b90bf6b691fb49335921
Koske Kimutai <koskedk@live.com>
2021-03-19 00:35:35 +0300

    manifest flow review

--- packages/server/src/application/metrices/commands/handlers/log-metric.handler.ts
+++ packages/server/src/application/metrices/commands/handlers/log-metric.handler.ts
@@ -1,224 +1,232 @@
 import {
-  CommandHandler,
-  EventBus,
-  EventPublisher,
-  ICommandHandler,
+    CommandHandler,
+    EventBus,
+    EventPublisher,
+    ICommandHandler,
 } from '@nestjs/cqrs';
-import { Inject, Logger } from '@nestjs/common';
+import {Inject, Logger} from '@nestjs/common';
 import {
-  Facility,
-  IDocketRepository,
-  IFacilityRepository,
-  IMasterFacilityRepository,
+    Facility,
+    IDocketRepository,
+    IFacilityRepository,
+    IMasterFacilityRepository,
 } from '../../../../domain';
 import * as uuid from 'uuid';
-import { plainToClass } from 'class-transformer';
-import { IMetricRepository } from '../../../../domain/metrices/metric-repository.interface';
-import { LogMetricCommand } from '../log-metric.command';
-import { MetricLoggedEvent } from '../../events/metric-logged.event';
-import { Metric } from '../../../../domain/metrices/metric';
-import { IMeasureRepository } from '../../../../domain/metrices/measure-repository.interface';
+import {plainToClass} from 'class-transformer';
+import {IMetricRepository} from '../../../../domain/metrices/metric-repository.interface';
+import {LogMetricCommand} from '../log-metric.command';
+import {MetricLoggedEvent} from '../../events/metric-logged.event';
+import {Metric} from '../../../../domain/metrices/metric';
+import {IMeasureRepository} from '../../../../domain/metrices/measure-repository.interface';
 
 @CommandHandler(LogMetricCommand)
 export class LogMetricHandler implements ICommandHandler<LogMetricCommand> {
-  constructor(
-    @Inject('IMasterFacilityRepository')
-    private readonly masterFacilityRepository: IMasterFacilityRepository,
-    @Inject('IDocketRepository')
-    private readonly docketRepository: IDocketRepository,
-    @Inject('IFacilityRepository')
-    private readonly facilityRepository: IFacilityRepository,
-    @Inject('IMetricRepository')
-    private readonly metricRepository: IMetricRepository,
-    @Inject('IMeasureRepository')
-    private readonly measureRepository: IMeasureRepository,
-    private readonly publisher: EventPublisher,
-    private readonly eventBus: EventBus,
-  ) {}
+    constructor(
+        @Inject('IMasterFacilityRepository')
+        private readonly masterFacilityRepository: IMasterFacilityRepository,
+        @Inject('IDocketRepository')
+        private readonly docketRepository: IDocketRepository,
+        @Inject('IFacilityRepository')
+        private readonly facilityRepository: IFacilityRepository,
+        @Inject('IMetricRepository')
+        private readonly metricRepository: IMetricRepository,
+        @Inject('IMeasureRepository')
+        private readonly measureRepository: IMeasureRepository,
+        private readonly publisher: EventPublisher,
+        private readonly eventBus: EventBus,
+    ) {
+    }
 
-  async execute(command: LogMetricCommand): Promise<any> {
-    try {
-      // check if metric Exists
-      const metricExists = await this.metricRepository.findByMetricId(
-        command.id,
-      );
-      if (metricExists) {
-        return;
-      }
+    async execute(command: LogMetricCommand): Promise<any> {
+        try {
+            // check if metric Exists
+            const metricExists = await this.metricRepository.findByMetricId(
+                command.id,
+            );
+            if (metricExists) {
+                return;
+            }
 
-      // check or enroll Facility
-      const facility = await this.enrollFacility(command);
+            // check or enroll Facility
+            const facility = await this.enrollFacility(command);
 
-      const newMetrics = await this.createMetrics(command);
-      for (const newMetric of newMetrics) {
+            if (!facility) {
+                return;
+            }
+
+            const newMetrics = await this.createMetrics(command);
+            for (const newMetric of newMetrics) {
+                if (facility) {
+                    newMetric.facility = facility._id;
+                    facility.addMetric(newMetric._id);
+                }
+
+                // log metric
+                const metric = await this.metricRepository.create(newMetric);
+                await this.metricRepository.updateCurrent(
+                    newMetric.facility,
+                    newMetric.measure,
+                );
+                this.publisher.mergeObjectContext(newMetric).commit();
+
+                const enrolledFacility = await this.facilityRepository.update(facility);
+                this.publisher.mergeObjectContext(facility).commit();
+
+                Logger.log(`Metric processed ${facility.name}`);
+
+                this.eventBus.publish(new MetricLoggedEvent(facility._id, metric._id));
+            }
+
+            return newMetrics;
+        } catch (e) {
+            Logger.error(e);
+        }
+        return undefined;
+    }
+
+    async enrollFacility(command: LogMetricCommand): Promise<Facility> {
+        const facility = await this.facilityRepository.findWithManifestByCode(
+            command.facilityCode,
+        );
+
         if (facility) {
-          newMetric.facility = facility._id;
-          facility.addMetric(newMetric._id);
+            return plainToClass(Facility, facility);
+        }
+        // TODO: metric arrives befor manifest
+        // return undefined;
+
+        const newFacility = new Facility(
+            uuid.v1(),
+            command.facilityCode,
+            command.facilityName,
+        );
+        newFacility.addManifest(command.facilityManifestId);
+        const masterFacility = await this.masterFacilityRepository.findByCode(
+            command.facilityCode,
+        );
+
+        if (masterFacility) {
+            newFacility.masterFacility = masterFacility;
+        }
+        const enrolledFacility = await this.facilityRepository.create(newFacility);
+        this.publisher.mergeObjectContext(newFacility).commit();
+        return newFacility;
+    }
+
+    createMetric(command: LogMetricCommand): Metric {
+        const measure = this.generateMeasure(command.cargoType, command.cargo);
+        const metric = new Metric(
+            command.id,
+            command.facilityCode,
+            command.facilityName,
+            measure,
+            command.facilityManifestId,
+        );
+        metric.reportDate = this.getReportDate(command.cargo);
+        return metric;
+    }
+
+    async createMetrics(command: LogMetricCommand): Promise<Metric[]> {
+        const metrics: Metric[] = [];
+        if (command.cargoType === 1) {
+            // EmrName
+            const metric1 = new Metric(
+                command.id,
+                '7eb13e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
+                command.facilityCode,
+                command.cargo.EmrName,
+                command.facilityManifestId,
+            );
+            metric1.reportDate = command.cargo.DateExtracted;
+            metrics.push(metric1);
+
+            // EmrVersion
+            const metric2 = new Metric(
+                command.id,
+                '7eb14e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
+                command.facilityCode,
+                command.cargo.EmrVersion,
+                command.facilityManifestId,
+            );
+            metric2.reportDate = command.cargo.DateExtracted;
+            metrics.push(metric2);
+
+            // LastLoginDate
+            const metric3 = new Metric(
+                command.id,
+                '7eb15e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
+                command.facilityCode,
+                command.cargo.LastLoginDate,
+                command.facilityManifestId,
+            );
+            metric3.reportDate = command.cargo.DateExtracted;
+            metrics.push(metric3);
+
+            // LastMOH731RunDate
+            const metric4 = new Metric(
+                command.id,
+                '7eb16e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
+                command.facilityCode,
+                command.cargo.LastMoH731RunDate,
+                command.facilityManifestId,
+            );
+            metric4.reportDate = command.cargo.DateExtracted;
+            metrics.push(metric4);
         }
 
-        // log metric
-        const metric = await this.metricRepository.create(newMetric);
-        await this.metricRepository.updateCurrent(
-          newMetric.facility,
-          newMetric.measure,
-        );
-        this.publisher.mergeObjectContext(newMetric).commit();
+        if (command.cargoType === 2) {
+            const measure = await this.generateMeasure(
+                command.cargoType,
+                command.cargo,
+            );
+            let metavalue = command.cargo.LogDate;
 
-        const enrolledFacility = await this.facilityRepository.update(facility);
-        this.publisher.mergeObjectContext(facility).commit();
+            if (measure === '7eb29e4a-bb7b-11e9-9cb5-2a2ae2dbcce4') {
+                metavalue = command.cargo.Version;
+            }
 
-        Logger.log(`Metric processed ${facility.name}`);
-
-        this.eventBus.publish(new MetricLoggedEvent(facility._id, metric._id));
-      }
-
-      return newMetrics;
-    } catch (e) {
-      Logger.error(e);
-    }
-    return undefined;
-  }
-
-  async enrollFacility(command: LogMetricCommand): Promise<Facility> {
-    const facility = await this.facilityRepository.findByCode(
-      command.facilityCode,
-    );
-
-    if (facility) {
-      return plainToClass(Facility, facility);
+            const metric = new Metric(
+                command.id,
+                measure,
+                command.facilityCode,
+                metavalue,
+                command.facilityManifestId,
+            );
+            metric.reportDate = command.cargo.LogDate;
+            const exisitngMetrics = metrics.filter(x => x._id === measure);
+            if (exisitngMetrics.length === 0) {
+                metrics.push(metric);
+            }
+        }
+        return metrics;
     }
 
-    const newFacility = new Facility(
-      uuid.v1(),
-      command.facilityCode,
-      command.facilityName,
-    );
-    const masterFacility = await this.masterFacilityRepository.findByCode(
-      command.facilityCode,
-    );
+    async generateMeasure(any: number, cargo: any): Promise<string> {
+        let area = cargo.Name;
+        let name;
 
-    if (masterFacility) {
-      newFacility.masterFacility = masterFacility;
-    }
-    const enrolledFacility = await this.facilityRepository.create(newFacility);
-    this.publisher.mergeObjectContext(newFacility).commit();
-    return newFacility;
-  }
+        if (cargo.Action === 'Sent') {
+            name = 'ExtractSent';
+        }
+        if (cargo.Action === 'Loaded') {
+            name = 'ExtractLoaded';
+        }
+        if (cargo.Name === 'MetricService') {
+            area = 'Dwapi';
+            name = 'DwapiVersion';
+        }
 
-  createMetric(command: LogMetricCommand): Metric {
-    const measure = this.generateMeasure(command.cargoType, command.cargo);
-    const metric = new Metric(
-      command.id,
-      command.facilityCode,
-      command.facilityName,
-      measure,
-      command.facilityManifestId,
-    );
-    metric.reportDate = this.getReportDate(command.cargo);
-    return metric;
-  }
-
-  async createMetrics(command: LogMetricCommand): Promise<Metric[]> {
-    const metrics: Metric[] = [];
-    if (command.cargoType === 1) {
-      // EmrName
-      const metric1 = new Metric(
-          command.id,
-          '7eb13e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
-          command.facilityCode,
-          command.cargo.EmrName,
-          command.facilityManifestId,
-      );
-      metric1.reportDate = command.cargo.DateExtracted;
-      metrics.push(metric1);
-
-      // EmrVersion
-      const metric2 = new Metric(
-          command.id,
-          '7eb14e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
-          command.facilityCode,
-          command.cargo.EmrVersion,
-          command.facilityManifestId,
-      );
-      metric2.reportDate = command.cargo.DateExtracted;
-      metrics.push(metric2);
-
-      // LastLoginDate
-      const metric3 = new Metric(
-          command.id,
-          '7eb15e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
-          command.facilityCode,
-          command.cargo.LastLoginDate,
-          command.facilityManifestId,
-      );
-      metric3.reportDate = command.cargo.DateExtracted;
-      metrics.push(metric3);
-
-      // LastMOH731RunDate
-      const metric4 = new Metric(
-          command.id,
-          '7eb16e4a-bb7b-11e9-9cb5-2a2ae2dbcce4',
-          command.facilityCode,
-          command.cargo.LastMoH731RunDate,
-          command.facilityManifestId,
-      );
-      metric4.reportDate = command.cargo.DateExtracted;
-      metrics.push(metric4);
+        const m = await this.measureRepository.getByName(area, name);
+        if (m) {
+            return m._id;
+        }
+        return '';
     }
 
-    if (command.cargoType === 2) {
-      const measure = await this.generateMeasure(
-          command.cargoType,
-          command.cargo,
-      );
-      let metavalue = command.cargo.LogDate;
-
-      if (measure === '7eb29e4a-bb7b-11e9-9cb5-2a2ae2dbcce4') {
-        metavalue = command.cargo.Version;
-      }
-
-      const metric = new Metric(
-          command.id,
-          measure,
-          command.facilityCode,
-          metavalue,
-          command.facilityManifestId,
-      );
-      metric.reportDate = command.cargo.LogDate;
-      const exisitngMetrics = metrics.filter(x => x._id === measure);
-      if (exisitngMetrics.length === 0) {
-        metrics.push(metric);
-      }
+    getReportDate(cargo: any) {
+        if (cargo.Action === 'Sent' || cargo.Action === 'Loaded') {
+            return cargo.LogDate;
+        }
+        return cargo.DateExtracted;
     }
-    return metrics;
-  }
-
-  async generateMeasure(any: number, cargo: any): Promise<string> {
-    let area = cargo.Name;
-    let name;
-
-    if (cargo.Action === 'Sent') {
-      name = 'ExtractSent';
-    }
-    if (cargo.Action === 'Loaded') {
-      name = 'ExtractLoaded';
-    }
-    if (cargo.Name === 'MetricService') {
-      area = 'Dwapi';
-      name = 'DwapiVersion';
-    }
-
-    const m = await this.measureRepository.getByName(area, name);
-    if (m) {
-      return m._id;
-    }
-    return '';
-  }
-
-  getReportDate(cargo: any) {
-    if (cargo.Action === 'Sent' || cargo.Action === 'Loaded') {
-      return cargo.LogDate;
-    }
-    return cargo.DateExtracted;
-  }
 }
--- packages/server/src/domain/transfers/facility.ts
+++ packages/server/src/domain/transfers/facility.ts
@@ -1,118 +1,124 @@
-import { AggregateRoot } from '@nestjs/cqrs';
-import { Manifest } from './manifest';
-import { Summary } from './summary';
-import { MasterFacility } from '../registries/master-facility';
-import { DocketCreatedEvent } from '../../application/courts/events';
-import { FacilityEnrolledEvent } from '../../application/transfers/events/facility-enrolled.event';
-import { ManifestLoggedEvent } from '../../application/transfers/events/manifest-logged.event';
-import { FacilityUpdatedEvent } from '../../application/transfers/events/facility-updated.event';
-import { FacilityStatsUpdatedEvent } from '../../application/transfers/events/facility-stats-updated.event';
-import { Metric } from '../metrices/metric';
+import {AggregateRoot} from '@nestjs/cqrs';
+import {Manifest} from './manifest';
+import {Summary} from './summary';
+import {MasterFacility} from '../registries/master-facility';
+import {DocketCreatedEvent} from '../../application/courts/events';
+import {FacilityEnrolledEvent} from '../../application/transfers/events/facility-enrolled.event';
+import {ManifestLoggedEvent} from '../../application/transfers/events/manifest-logged.event';
+import {FacilityUpdatedEvent} from '../../application/transfers/events/facility-updated.event';
+import {FacilityStatsUpdatedEvent} from '../../application/transfers/events/facility-stats-updated.event';
+import {Metric} from '../metrices/metric';
 import {Docket} from '../courts/docket';
 
 export class Facility extends AggregateRoot {
-  manifests?: any[] = [];
-  summaries?: Summary[] = [];
-  masterFacility?: MasterFacility;
-  metrics?: any[] = [];
+    manifests?: any[] = [];
+    summaries?: Summary[] = [];
+    masterFacility?: MasterFacility;
+    metrics?: any[] = [];
 
-  constructor(public _id: string, public code: number, public name: string) {
-    super();
-    this.apply(new FacilityEnrolledEvent(this._id));
-  }
-
-  addManifest(manifestId: string) {
-    this.manifests.push(manifestId);
-    this.apply(new FacilityUpdatedEvent(this._id));
-  }
-
-  addMetric(metricId: string) {
-    this.metrics.push(metricId);
-    this.apply(new FacilityUpdatedEvent(this._id));
-  }
-
-  assignMasterFacility(masterFacility: MasterFacility) {
-    this.masterFacility = masterFacility;
-    this.apply(new FacilityUpdatedEvent(this._id));
-  }
-
-  summaryHasExtract(_id: string) {
-    const extract = this.summaries.find((s) => s.extract._id === _id);
-    if (extract) {
-      return true;
+    constructor(public _id: string, public code: number, public name: string) {
+        super();
+        this.apply(new FacilityEnrolledEvent(this._id));
     }
-    return false;
-  }
 
-  addSummary(summary: Summary) {
-    this.summaries.push(summary);
-    this.apply(new FacilityStatsUpdatedEvent(this._id));
-  }
-
-  updateSummary(docket: any, stats: any, updated: Date) {
-    this.summaries
-      .filter((s) => s.docket.name === docket.name)
-      .forEach((ss) => {
-        const stat = stats.find((x) => x.name === ss.extract.name ||
-            x.name === `${ss.extract.name}Extract` ||
-            x.name === ss.extract.name.replace('Extract', ''));
-        if (stat) {
-          ss.recieved = stat.recieved;
-          ss.updated = stat.updated;
+    addManifest(manifestId: string) {
+        if (this.manifests && this.manifests.length > 0) {
+            const existing = this.manifests.filter(x => x === manifestId)[0];
+            if (existing) {
+                return;
+            }
         }
-      });
-    this.apply(new FacilityStatsUpdatedEvent(this._id));
-  }
-
-  getPatientSummary(docket: string): any {
-    const psum = this.summaries.find(
-      (s) => s.docket.name === docket && s.extract.isPatient,
-    );
-    if (psum) {
-      return psum.recieved;
-    }
-    return null;
-  }
-
-  resetSummary(_id: string, expected: any, updated: Date) {
-    this.summaries.forEach((s) => {
-      if (s.extract._id === _id) {
-        s.expected = expected;
-        s.recieved = 0;
-        s.updated = updated;
-      }
-    });
-  }
-
-  getStats(docket: string, extract: string) {
-    const s = this.summaries.find((f) => f.extract.name === extract);
-    if (s) {
-      return s;
-    }
-    return undefined;
-  }
-
-  hasSummaries(): boolean {
-    if (!this.summaries) {
-      return false;
+        this.manifests.push(manifestId);
+        this.apply(new FacilityUpdatedEvent(this._id));
     }
 
-    if (this.summaries && this.summaries.length === 0) {
-      return false;
+    addMetric(metricId: string) {
+        this.metrics.push(metricId);
+        this.apply(new FacilityUpdatedEvent(this._id));
     }
 
-    return true;
-  }
-
-  hasDocketSummaries(docket: string): boolean {
-    if (!this.summaries) {
-      return false;
+    assignMasterFacility(masterFacility: MasterFacility) {
+        this.masterFacility = masterFacility;
+        this.apply(new FacilityUpdatedEvent(this._id));
     }
 
-    if (this.summaries && this.summaries.length > 0) {
-      const ds = this.summaries.filter(xx => xx.docket.name === docket);
-      return ds.length > 0;
+    summaryHasExtract(_id: string) {
+        const extract = this.summaries.find((s) => s.extract._id === _id);
+        if (extract) {
+            return true;
+        }
+        return false;
     }
-    return true;
-  }
+
+    addSummary(summary: Summary) {
+        this.summaries.push(summary);
+        this.apply(new FacilityStatsUpdatedEvent(this._id));
+    }
+
+    updateSummary(docket: any, stats: any, updated: Date) {
+        this.summaries
+            .filter((s) => s.docket.name === docket.name)
+            .forEach((ss) => {
+                const stat = stats.find((x) => x.name === ss.extract.name ||
+                    x.name === `${ss.extract.name}Extract` ||
+                    x.name === ss.extract.name.replace('Extract', ''));
+                if (stat) {
+                    ss.recieved = stat.recieved;
+                    ss.updated = stat.updated;
+                }
+            });
+        this.apply(new FacilityStatsUpdatedEvent(this._id));
+    }
+
+    getPatientSummary(docket: string): any {
+        const psum = this.summaries.find(
+            (s) => s.docket.name === docket && s.extract.isPatient,
+        );
+        if (psum) {
+            return psum.recieved;
+        }
+        return null;
+    }
+
+    resetSummary(_id: string, expected: any, updated: Date) {
+        this.summaries.forEach((s) => {
+            if (s.extract._id === _id) {
+                s.expected = expected;
+                s.recieved = 0;
+                s.updated = updated;
+            }
+        });
+    }
+
+    getStats(docket: string, extract: string) {
+        const s = this.summaries.find((f) => f.extract.name === extract);
+        if (s) {
+            return s;
+        }
+        return undefined;
+    }
+
+    hasSummaries(): boolean {
+        if (!this.summaries) {
+            return false;
+        }
+
+        if (this.summaries && this.summaries.length === 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    hasDocketSummaries(docket: string): boolean {
+        if (!this.summaries) {
+            return false;
+        }
+
+        if (this.summaries && this.summaries.length > 0) {
+            const ds = this.summaries.filter(xx => xx.docket.name === docket);
+            return ds.length > 0;
+        }
+        return true;
+    }
 }
