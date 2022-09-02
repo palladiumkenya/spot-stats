e12a79ce3a40264137bc96a773f06c35f5761f52
cbrianbet <cbrianbet@outlook.com>
2022-09-02 12:05:50 +0300

    Fixed bug that checks manifest before update

--- packages/server/src/application/transfers/commands/handlers/log-manifest.handler.ts
+++ packages/server/src/application/transfers/commands/handlers/log-manifest.handler.ts
@@ -36,7 +36,9 @@ export class LogManifestHandler implements ICommandHandler<LogManifestCommand> {
 
   async execute(command: LogManifestCommand): Promise<any> {
     // check if manifest Exists
-    const manifestExists = await this.manifestRepository.get(command.id);
+    const manifestExists = await this.manifestRepository.getByManifestId(
+      command.id,
+    );
     if (manifestExists) {
       return;
     }
