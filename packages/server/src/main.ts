032e36dcb9e50537f89f597d4852eff7ba063a74
cbrianbet <cbrianbet@outlook.com>
2022-10-14 15:43:21 +0300

    cron job and api to update sessions added

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,6 +6,8 @@ import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import { Logger } from '@nestjs/common';
 import { urlencoded, json } from 'express';
+import { CronJob } from 'cron';
+import { TransfersModule } from './application/transfers/transfers.module';
 
 async function bootstrap() {
   const config = new ConfigService(`${process.env.NODE_ENV}.env`);
@@ -21,6 +23,20 @@ async function bootstrap() {
   app.enableCors();
   const seeder = app.get(SeederModule);
   await seeder.seedData();
+
+  // cron job that runs every 7pm
+  let cronJob = new CronJob('0 19 * * *', async () => {
+    try {
+      app.get(TransfersModule).update();
+    } catch (e) {
+      Logger.error('---- FAILED TO UPDATE SESSION ----');
+    }
+  });
+  if (!cronJob.running) {
+    //starts cron job if it isnt running
+    cronJob.start();
+  }
+  
   await app.listen(config.Port);
 }
 
