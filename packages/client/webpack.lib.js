e2b42e44324403749c2b31775ee497d941c8ccc5
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-10-26 16:28:55 +0300

    updated webpack

--- packages/client/webpack.lib.js
+++ packages/client/webpack.lib.js
@@ -6,7 +6,7 @@ const webpack = require("webpack");
 
 common.devServer.proxy = {
   "/api/": {
-    target: "https://localhost:4720",
+    target: "https://localhost:4702",
     pathRewrite: { "^/api": "" }
   }
 };
